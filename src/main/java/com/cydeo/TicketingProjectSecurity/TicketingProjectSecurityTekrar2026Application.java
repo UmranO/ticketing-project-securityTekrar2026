746a1e1ed684fd4b3ec526fb617532cc07498b0f
Umran Ozman <umonal@yahoo.com>
2026-01-12 09:33:16 -0500

    TicketingProjectSecurityTekrar2026Application created

---
+++ src/main/java/com/cydeo/TicketingProjectSecurity/TicketingProjectSecurityTekrar2026Application.java
@@ -0,0 +1,13 @@
+package com.cydeo.TicketingProjectSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectSecurityTekrar2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectSecurityTekrar2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cydeo/TicketingProjectSecurity/TicketingProjectSecurityTekrar2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.TicketingProjectSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingProjectSecurityTekrar2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
